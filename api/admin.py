8e33c2eae6bc099be1fce6347639b94a920ef7db
John Luke <joaolucas.deoliveira56@gmail.com>
2023-11-23 21:03:29 -0300

    fix: add PlaylistAdminForm and PlaylistAdmin to api/admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,19 @@
+from django import forms
 from django.contrib import admin
 from .models import Phrase, Category, Playlist
 
 
-admin.site.register(Playlist)
+class PlaylistAdminForm(forms.ModelForm):
+    class Meta:
+        model = Playlist
+        fields = '__all__'
+        widgets = {
+            'phrases': forms.CheckboxSelectMultiple,
+        }
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    form = PlaylistAdminForm
+
 admin.site.register(Phrase)
 admin.site.register(Category)
